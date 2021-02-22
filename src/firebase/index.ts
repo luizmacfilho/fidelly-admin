58551565c591f7970873c9d6a35bf4d3fc62761f
Luiz Filho <luiz.macfilho@gmail.com>
2021-02-22 20:04:40 -0300

    adding ads

---
+++ src/firebase/index.ts
@@ -0,0 +1,20 @@
+export const mobileConfig = {
+    apiKey: "AIzaSyCkvvm15u-DjYmY8zW6Kkp1XEkpSPmhRes",
+  authDomain: "fidelly-mob.firebaseapp.com",
+  databaseURL: "https://fidelly-mob.firebaseio.com",
+  projectId: "fidelly-mob",
+  storageBucket: "fidelly-mob.appspot.com",
+  messagingSenderId: "193348783485",
+  appId: "1:193348783485:web:9281039ea5c825ac381014",
+  measurementId: "G-JNV1KZFNJY",
+}
+
+export const adminConfig = {
+    apiKey: "AIzaSyCpo9jBhNYPrffgWX0uOh2pDrcJZV4fGfI",
+    authDomain: "fidelly-app.firebaseapp.com",
+    databaseURL: "https://fidelly-app.firebaseio.com",
+    projectId: "fidelly-app",
+    storageBucket: "fidelly-app.appspot.com",
+    messagingSenderId: "370012281710",
+    appId: "1:370012281710:web:dfdf2c5d1a0662b6"
+  };
